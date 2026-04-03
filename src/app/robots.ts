c953d8799d250cab92fadc88ea002ef734cf2a09
giorgi khimshiashvili <giorgikhimshiashvili@gmail.com>
2026-04-03 11:00:58 +0200

    feat: refactor result page structure; add ResultClient component and update SEO metadata

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      disallow: ["/private/", "/_next/static/", "/icon"],
     },
     sitemap: "https://importespana.com/sitemap.xml",
   };
